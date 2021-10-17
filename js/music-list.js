fb70593d89f2d4fcbbc1534c4d7750b6a608b081
iqbalarya98 <tumbalantumbalin@gmail.com>
2021-10-17 20:23:55 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Nothing's Gonna Change My Love For You",
+    artist: "Shania Yan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "First Love",
+    artist: "Shania Yan",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "I Swear",
+    artist: "Shania Yan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Imagination",
+    artist: "Shania Yan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Sentuh Hatiku",
+    artist: "Shania Yan",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Perfect",
+    artist: "Shania Yan",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
